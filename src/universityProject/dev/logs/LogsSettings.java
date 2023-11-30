92fb9ab81344c6ab3c3a941bd0dcdca17d91f018
Arslan <alan.aquinasss@gmail.com>
2023-11-30 15:06:03 +0600

    add methods to log

--- src/universityProject/dev/logs/LogsSettings.java
+++ src/universityProject/dev/logs/LogsSettings.java
@@ -1,46 +1,85 @@
 package universityProject;
 
+import java.io.*;
 
 /**
-* @generated
-*/
+ * @generated
+ */
 public class LogsSettings {
-    
-    /**
-    * @generated
-    */
-    private logRecord logs;
-    
-    
-    
 
     /**
-    * @generated
-    */
-    private logRecord getLogs() {
-        return this.logs;
-    }
-    
+     * @generated
+     */
+    private static LogsSettings instance;
+
     /**
-    * @generated
-    */
-    private logRecord setLogs(logRecord logs) {
+     * @generated
+     */
+    private static logRecord logs;
+
+    private LogsSettings() {
+        // Private constructor to prevent instantiation
+    }
+
+    /**
+     * Get the singleton instance of LogsSettings
+     *
+     * @return The singleton instance
+     */
+    public static synchronized LogsSettings getInstance() {
+        if (instance == null) {
+            instance = new LogsSettings();
+        }
+        return instance;
+    }
+
+    /**
+     * Add a log using serialization
+     *
+     * @param logRecord The log record to be added
+     */
+    public void addLog(logRecord logRecord) {
+        // Deserialize existing logs
+        logRecord existingLogs = retrieveLogs();
+
+        // If there are existing logs, append the new log
+        if (existingLogs != null) {
+            // Assuming your logRecord class has a method to append logs
+            existingLogs.appendLog(logRecord);
+            setLogs(existingLogs);
+        } else {
+            // If there are no existing logs, set the new log
+            setLogs(logRecord);
+        }
+
+        // Serialize and save the logs
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("logs.ser"))) {
+            oos.writeObject(getLogs());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Retrieve logs from serialization
+     *
+     * @return The deserialized log record
+     */
+    public logRecord retrieveLogs() {
+        // Deserialize and return the log record
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("logs.ser"))) {
+            setLogs((logRecord) ois.readObject());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return getLogs();
+    }
+
+    private logRecord getLogs() {
+        return logs;
+    }
+
+    private void setLogs(logRecord logs) {
         this.logs = logs;
     }
-    
-    
-    
-    
-
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
-    public logRecord getLogs() {
-        //TODO
-        return null;
-    }
-    
-    
 }
--- src/universityProject/dev/logs/logRecord.java
+++ src/universityProject/dev/logs/logRecord.java
@@ -25,9 +25,13 @@ public class logRecord {
     * @generated
     */
     private User user;
-    
-    
-    
+
+    public logRecord(int logId, String text, String time, User user) {
+        this.logId = logId;
+        this.text = text;
+        this.time = time;
+        this.user = user;
+    }
 
     /**
     * @generated
@@ -39,7 +43,7 @@ public class logRecord {
     /**
     * @generated
     */
-    private int setLogId(Integer logId) {
+    private void setLogId(Integer logId) {
         this.logId = logId;
     }
     
@@ -54,7 +58,7 @@ public class logRecord {
     /**
     * @generated
     */
-    private String setText(String text) {
+    private void setText(String text) {
         this.text = text;
     }
     
@@ -69,7 +73,7 @@ public class logRecord {
     /**
     * @generated
     */
-    private String setTime(String time) {
+    private void setTime(String time) {
         this.time = time;
     }
     
@@ -84,11 +88,14 @@ public class logRecord {
     /**
     * @generated
     */
-    private User setUser(User user) {
+    private void setUser(User user) {
         this.user = user;
     }
-    
-    
-    
-    
+
+    public void appendLog(logRecord logRecord) {
+        this.text += "\n" + logRecord.getText();
+
+        // Append time (you might want to handle time in a more sophisticated way)
+        this.time += "\n" + logRecord.getTime();
+    }
 }
