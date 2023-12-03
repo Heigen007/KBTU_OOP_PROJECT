5c5480c15a2d64c9f06a8e24b7eff25b6931e57b
asylken11 <124186460+asylken11@users.noreply.github.com>
2023-12-03 18:12:38 +0600

    Update News.java

--- src/universityProject/dev/academicEntities/News.java
+++ src/universityProject/dev/academicEntities/News.java
@@ -5,7 +5,7 @@ public class News {
     private int newsID;
     private String title;
     private String content;
-    private String comments;
+    private List<String> comments;
 
     // Конструктор без параметров
     public News() {
@@ -53,12 +53,62 @@ public class News {
         this.comments = comments;
     }
 
-    // Операции
 
-    public String editContent() {
-        //Нужно реализовать логику редактирования контента
-        return "";
+// Operations
+public void publishNews(String newContent, String newComments) {
+        if (isValidContent(newContent) && isValidComment(newComments)) {
+            System.out.println("News published or updated successfully.");
+            System.out.println("Old content: " + content + ", New content: " + newContent);
+            System.out.println("Old comments: " + comments + ", New comments: " + newComments);
+            this.content = newContent;
+            this.comments = newComments;
+        } else {
+            System.out.println("Invalid content or comments provided. News not published or updated.");
+        }
     }
+
+    // Validation method for content
+    private boolean isValidContent(String newContent) {
+        return newContent != null && !newContent.trim().isEmpty() && newContent.length() <= 1000;
+    }
+
+    // Validation method for comments
+    private boolean isValidComment(String newComment) {
+        return newComment != null && !newComment.trim().isEmpty() && newComment.length() <= 500;
+    }
+
+ // Operation to edit the content of the news article with validation
+    public void editContent(String newContent) {
+        if (isValidContent(newContent)) {
+            this.content = newContent;
+            System.out.println("Content edited successfully.");
+        } else {
+            System.out.println("Invalid content. Please provide valid content.");
+        }
+    }
+
+    // Validation method for content
+    private boolean isValidContent(String newContent) {
+        return newContent != null && !newContent.trim().isEmpty() && newContent.length() <= 1000;
+    }
+ // Operation to add a comment to the news article
+    public void addComment(String newComment) {
+        if (isValidComment(newComment)) {
+            if (comments == null) {
+                comments = new ArrayList<>();
+            }
+            comments.add(newComment);
+            System.out.println("Comment added successfully.");
+        } else {
+            System.out.println("Invalid comment. Please provide a valid comment.");
+        }
+    }
+
+    // Validation method for comments
+    private boolean isValidComment(String newComment) {
+        return newComment != null && !newComment.trim().isEmpty() && newComment.length() <= 10;
+    }
+ 
 
 
 }
