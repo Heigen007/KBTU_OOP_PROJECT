630d50599e704596b694fd40412f381b2a94433c
asylken11 <124186460+asylken11@users.noreply.github.com>
2023-12-18 18:31:18 +0600

    Add files via upload

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"universityProject.dev.research"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"universityProject.dev.research","l":"ResearchProject"}];updateSearchResults();
