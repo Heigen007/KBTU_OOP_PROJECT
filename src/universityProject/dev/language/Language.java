ff7442492d57f9cd2f529bf64c752b8b81884523
asylken11 <124186460+asylken11@users.noreply.github.com>
2023-12-19 13:49:51 +0600

    Update Language.java

--- src/universityProject/dev/language/Language.java
+++ src/universityProject/dev/language/Language.java
@@ -1,5 +1,22 @@
 package universityProject.dev.language;
 
-public enum Language  {
-    RU, KZ, EN
+/**
+ * The Language enum represents the available language options for the application.
+ * Each enum constant corresponds to a specific language.
+ */
+public enum Language {
+    /**
+     * Represents the Russian language.
+     */
+    RU,
+
+    /**
+     * Represents the Kazakh language.
+     */
+    KZ,
+
+    /**
+     * Represents the English language.
+     */
+    EN
 }
