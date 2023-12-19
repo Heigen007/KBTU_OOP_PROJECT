75ddf7c88dad059c3de32aa03a402eecd49b0609
asylken11 <124186460+asylken11@users.noreply.github.com>
2023-12-19 13:50:48 +0600

    Update LanguageSettings.java

--- src/universityProject/dev/language/LanguageSettings.java
+++ src/universityProject/dev/language/LanguageSettings.java
@@ -3,14 +3,26 @@ package universityProject.dev.language;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+/**
+ * The LanguageSettings class manages the language settings for the application.
+ * It provides methods to set the current language and retrieve localized strings.
+ */
 public class LanguageSettings {
     private static LanguageSettings instance;
     private Language currentLanguage;
     private static ResourceBundle resourceBundle;
 
+    /**
+     * Private constructor to enforce the singleton pattern.
+     */
     private LanguageSettings() {
     }
 
+    /**
+     * Retrieves the singleton instance of the LanguageSettings class.
+     *
+     * @return The singleton instance of LanguageSettings.
+     */
     public static LanguageSettings getInstance() {
         if (instance == null) {
             instance = new LanguageSettings();
@@ -18,10 +30,21 @@ public class LanguageSettings {
         return instance;
     }
 
+    /**
+     * Retrieves the current language.
+     *
+     * @return The current language.
+     */
     private Language getCurrentLanguage() {
         return this.currentLanguage;
     }
 
+    /**
+     * Sets the current language based on the provided language code.
+     * Default language is English if the provided code is not recognized.
+     *
+     * @param languageCode The language code (e.g., "RU", "KZ", "EN").
+     */
     public static void setCurrentLanguage(String languageCode) {
         Locale currentLocale = switch (languageCode) {
             case "RU" -> new Locale("ru", "RU");
@@ -31,6 +54,12 @@ public class LanguageSettings {
         resourceBundle = ResourceBundle.getBundle("Messages", currentLocale);
     }
 
+    /**
+     * Retrieves the localized string for the given key.
+     *
+     * @param key The key corresponding to the desired localized string.
+     * @return The localized string.
+     */
     public String getString(String key) {
         return resourceBundle.getString(key);
     }
