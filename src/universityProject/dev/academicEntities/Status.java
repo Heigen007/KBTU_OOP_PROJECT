37009b68e1de008a6f0c86a2e2fea1569e08e187
asylken11 <124186460+asylken11@users.noreply.github.com>
2023-12-19 13:42:58 +0600

    Update Status.java

--- src/universityProject/dev/academicEntities/Status.java
+++ src/universityProject/dev/academicEntities/Status.java
@@ -1,5 +1,18 @@
 package universityProject.dev.academicEntities;
 
+/**
+ * @author Asyl
+ * @version 18.12.2023
+ * The Status enum represents the status of an order, which can be CREATED, SOLVED, or REJECTED.
+ */
 public enum Status {
-    CREATED, SOLVED, REJECTED
+
+    /** Represents an order that has been created but not yet resolved. */
+    CREATED,
+
+    /** Represents an order that has been successfully solved. */
+    SOLVED,
+
+    /** Represents an order that has been rejected or could not be resolved. */
+    REJECTED
 }
